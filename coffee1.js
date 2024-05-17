886aec28437679b8a6acde61cd45ae03c21fdc11
Nishant-saini-12 <158986060+Nishant-saini-12@users.noreply.github.com>
2024-05-17 09:00:02 +0530

    Delete coffee1.js

--- coffee1.js
+++
@@ -1,9 +0,0 @@
-// script.js
-
-function openModal(modalId) {
-    document.getElementById(modalId).style.display = 'flex';
-}
-
-function closeModal(modalId) {
-    document.getElementById(modalId).style.display = 'none';
-}
